138a263fc0030c6b610c2cbaabb5cd578086c420
Eduardo S. Mesquita <102036752+Duzoka@users.noreply.github.com>
2023-02-02 20:39:48 -0300

    Added onclick and function to write the code on p

--- src/js/JavaScript-Calculator.js
+++ src/js/JavaScript-Calculator.js
@@ -1,7 +1,20 @@
+
+// Função para inserir os valores na tela
+function insert (num) {
+
+//  Olhar o documento, pegar o Elemento por ID e inserir o resultado no HTML igual a "num"
+  var numero = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = numero + num;
+
+}
+
+
 const buttons = document.querySelectorAll('.number-button, .operator-button');
 buttons.forEach(button => {
   button.addEventListener('click', event => {
     const value = event.target.textContent;
+    
+
     console.log(`Botão clicado: ${value}`);
     // aqui você pode adicionar o código para fazer o cálculo com base no valor do botão clicado
   });
