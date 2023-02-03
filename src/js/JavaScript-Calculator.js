32b4d7d3c65ab5c80db1ce1e91ebe0b3e0f72b20
Eduardo S. Mesquita <102036752+Duzoka@users.noreply.github.com>
2023-02-02 23:22:58 -0300

    Added the last code to js to make AC and BC and "." works

--- src/js/JavaScript-Calculator.js
+++ src/js/JavaScript-Calculator.js
@@ -1,21 +1,33 @@
 
 // Função para inserir os valores na tela
-function insert (num) {
+function insert (num)
+{
 
-//  Olhar o documento, pegar o Elemento por ID e inserir o resultado no HTML igual a "num"
+// "Variavel" irá ser igual ao valor que ao olhar o "documento", pegará o "Elemento por ID" e inserir dentro do HTML de id resultado igual a "num"
   var numero = document.getElementById('resultado').innerHTML;
   document.getElementById('resultado').innerHTML = numero + num;
 
 }
 
+//Limpar Tudo
+function ac () {
+  document.getElementById('resultado').innerHTML = "";
+}
 
-const buttons = document.querySelectorAll('.number-button, .operator-button');
-buttons.forEach(button => {
-  button.addEventListener('click', event => {
-    const value = event.target.textContent;
-    
+//Limpar somente o ultimo numero
+function bc () {
+  var resultado = document.getElementById("resultado").innerHTML;
+  document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
 
-    console.log(`Botão clicado: ${value}`);
-    // aqui você pode adicionar o código para fazer o cálculo com base no valor do botão clicado
-  });
-});
+function calc () {
+  var resultado = document.getElementById("resultado").innerHTML;
+  if(resultado)
+  {
+    document.getElementById('resultado').innerHTML = eval(resultado);
+  }
+  else
+  {
+    document.getElementById("resultado").innerHTML = ""
+  }
+}
